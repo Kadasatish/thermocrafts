14d18a5c254967c877bebd0e9efb2e358b0ae51c
Kadasatish <kadasatish2022@gmail.com>
2025-07-03 20:31:55 +0530

    Delete js/main.js

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// /js/main.js
-function showPaymentSuccess(dotId) {
-  const dot = document.getElementById(dotId);
-  dot.style.backgroundColor = 'white';
-  setTimeout(() => {
-    dot.style.backgroundColor = 'black';
-  }, 1500);
-}
